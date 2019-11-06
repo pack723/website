a02a9db91b2a3f594a59e3d0859b04af7be1fd7e
mlamont8 <mlamont8@hotmail.com>
2019-11-06 15:36:33 -0500

    cleanup of layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,49 +1,16 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
-
-import PropTypes from "prop-types"
-// import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
-// import "./layout.css"
-import "../styles/global.scss"
 import Nav from "./nav"
 import Footer from "./footer"
+import "../styles/global.scss"
 
 const Layout = props => {
-  // const data = useStaticQuery(graphql`
-  //   query SiteTitleQuery {
-  //     site {
-  //       siteMetadata {
-  //         title
-  //       }
-  //     }
-  //   }
-  // `)
-
   return (
     <>
-      {/* <Header
-        title={props.title}
-        subtitle={props.subtitle}
-        size={props.size}
-        imageUrl={props.imageUrl}
-      /> */}
       <Nav />
       <main>{props.children}</main>
       <Footer />
     </>
   )
-}
-
-Layout.propTypes = {
-  children: PropTypes.node.isRequired,
 }
 
 export default Layout
