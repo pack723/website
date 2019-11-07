b12249e09fe40b857523cc9802b5e6adbbecf4a0
mlamont8 <mlamont8@hotmail.com>
2019-11-07 14:07:32 -0500

    added about page

---
+++ src/pages/about.js
@@ -0,0 +1,65 @@
+import React from "react"
+import Layout from "../components/layout"
+
+const About = () => (
+  <Layout>
+    <section className="hero is-primary">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">About Scouting</h1>
+        </div>
+      </div>
+    </section>
+    <section className="section">
+      <div className="container">
+        <p>
+          Cub Scouting is a family and home-centered program for boys and girls
+          in the first through fifth grades. Fourth and fifth grade (10 years
+          old) boys are called Webelos Scouts and participate in more advanced
+          activities that begin to prepare them to become Scouts. Cub Scouts get
+          to wear a uniform, go to places like summer camp and day camp. They
+          build racecars, make new friends and learn all sorts of new and
+          exciting things.
+        </p>
+      </div>
+    </section>
+    <section className="section">
+      <div className="container">
+        <h1 className="title">Mission</h1>
+        <p>
+          The mission of the Boy Scouts of America is to prepare young people to
+          make ethical and moral choices over their lifetimes by instilling in
+          them the values of the Scout Oath and Law.
+        </p>
+        <p>
+          There are also other Pack sponsored events, meetings, get togethers
+          and trips. Our Pack also makes an effort to participate in service
+          projects within the community throughout the year.
+        </p>
+      </div>
+    </section>
+
+    <section className="section">
+      <div className="container">
+        <h1 className="title">How Often Do We Meet?</h1>
+        <p>
+          Groups of boys of the same age/grade level called Dens generally meet
+          every Monday during the school year. Lion Scouts (Kindergarteners)
+          meet once a month on their own and sometimes with the Pack with some
+          of its activities. Full ranks of Cub Scouting begin with Tiger Scouts
+          (first-graders), then progress to Wolf Cub Scouts (second-graders),
+          Bear Cub Scouts (third-graders), Webelos Scouts (fourth graders) and
+          Arrow of Light (fifth-graders). Den meetings are held at Bethel AME
+          Church on 200 N Regan St.
+        </p>
+        <p>
+          There are also other Pack sponsored events, meetings, get togethers
+          and trips. Our Pack also makes an effort to participate in service
+          projects within the community throughout the year.
+        </p>
+      </div>
+    </section>
+  </Layout>
+)
+
+export default About
